42abab88bd97e8acc258b903f7094bfcaf004a5a
sdm <dongming.shen@sap.com>
2022-01-26 20:10:18 +0800

    init

---
+++ demo-conflicts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo-conflicts
+//
+//  Created by Shen, Dongming on 2022/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demo-conflicts/demo_conflictsApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_conflictsApp.swift
+//  demo-conflicts
+//
+//  Created by Shen, Dongming on 2022/1/26.
+//
+
+import SwiftUI
+
+@main
+struct demo_conflictsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
